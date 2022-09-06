05de9aee518942f2bbf90e88297c8bfceb2f57b4
Beetroot Paul <paul@beetrootpaul.com>
2022-09-06 22:52:20 +0200

    mockup: HUD adjustments

--- src/common/health.lua
+++ src/common/health.lua
@@ -2,6 +2,11 @@
 -- common/health.lua --
 -- -- -- -- -- -- -- --
 
+-- TODO: implement default shockwaves and powerup to gather them 
+-- TODO: implement faster shooting powerup 
+-- TODO: max healths and what then?
+-- TODO: max bombs and what then?
+
 -- TODO: balancing
 _health_default = 5
 -- DEBUG:
