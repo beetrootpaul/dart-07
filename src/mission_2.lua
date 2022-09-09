fa6184a4f06de3b5ceb9968369ae7a50e74c1864
Beetroot Paul <paul@beetrootpaul.com>
2022-09-10 00:05:16 +0200

    particles on levels 2 and 3

--- src/mission_2.lua
+++ src/mission_2.lua
@@ -4,84 +4,126 @@
 
 _m = {
     mission_number = 2,
-    scroll_per_frame = 1,
+    scroll_per_frame = .75,
     bg_color = _color_1_dark_blue,
-    has_bg_tiles = false,
     mission_name = "death space",
     boss_name = "cheerful death"
 }
 
--- TODO: polish mission 2 enemy bullets
-_m.enemy_bullet_factory = new_enemy_bullet_factory {
-    bullet_sprite = new_static_sprite(4, 4, 124, 64),
-    collision_circle_r = 1.5,
-}
+do
+    local stars
 
-function _m.enemy_properties_for(enemy_map_marker)
-    if enemy_map_marker == 79 then
-        return {
-            health = 1,
-            ship_sprite = new_static_sprite(8, 8, 0, 64),
-            collision_circle_r = 3.5,
-            collision_circle_offset_y = 0,
-            movement_factory = new_movement_sequence_factory {
-                new_movement_line_factory {
-                    frames = 80,
-                    angle = .75,
-                    angled_speed = .5,
-                },
-                new_movement_line_factory {
-                    angle = .75,
-                    angled_speed = 1.5,
-                },
-            },
-            bullet_fire_timer = new_timer(60),
-            spawn_bullets = function(enemy_movement, player_collision_circle)
-                local bullets = {}
-                for i = 4, 4 do
-                    add(bullets, _m.enemy_bullet_factory(
-                        new_movement_line_factory {
-                            base_speed_y = enemy_movement.speed_xy.y,
-                            angle = .5 + i / 16,
-                            angled_speed = 2,
-                        }(enemy_movement.xy)
-                    ))
-                end
-                return bullets
-            end,
-            powerups_distribution = "t",
-        }
+    local function maybe_add_star(y)
+        if rnd() < .2 then
+            add(stars, {
+                x = ceil(.1 + rnd(_gaw - .1)),
+                y = y,
+                speed = rnd { .3, .4, .5 }
+            })
+        end
     end
-    assert(false, "unexpected enemy_map_marker = " .. enemy_map_marker)
-end
 
-function _m.boss_properties()
-    return {
-        health = 50,
-        sprite = new_static_sprite(56, 26, 4, 98),
-        collision_circles = function(movement)
+    function _m.level_bg_init()
+        stars = {}
+
+        for y = 0, _gah - 1 do
+            maybe_add_star(y)
+        end
+    end
+
+    function _m.level_bg_update()
+        for _, star in pairs(stars) do
+            star.y = star.y + star.speed
+            if star.y >= _gah then
+                del(stars, star)
+            end
+        end
+
+        maybe_add_star(0)
+    end
+
+    function _m.level_bg_draw(min_visible_distance, max_visible_distance)
+        for _, star in pairs(stars) do
+            pset(
+                _gaox + star.x,
+                star.y,
+                star.speed > .45 and _color_7_white or (star.speed > .35 and _color_6_light_grey or _color_14_lavender))
+        end
+    end
+
+    -- TODO: polish mission 2 enemy bullets
+    local enemy_bullet_factory = new_enemy_bullet_factory {
+        bullet_sprite = new_static_sprite(4, 4, 124, 64),
+        collision_circle_r = 1.5,
+    }
+
+    function _m.enemy_properties_for(enemy_map_marker)
+        if enemy_map_marker == 79 then
             return {
-                { xy = movement.xy.plus(0, 3), r = 5 },
-            }
-        end,
-        phases = {
-            -- phase 1:
-            {
-                triggering_health_fraction = 1,
-                bullet_fire_timer = new_timer(80),
+                health = 1,
+                ship_sprite = new_static_sprite(8, 8, 0, 64),
+                collision_circle_r = 3.5,
+                collision_circle_offset_y = 0,
+                movement_factory = new_movement_sequence_factory {
+                    new_movement_line_factory {
+                        frames = 80,
+                        angle = .75,
+                        angled_speed = .5,
+                    },
+                    new_movement_line_factory {
+                        angle = .75,
+                        angled_speed = 1.5,
+                    },
+                },
+                bullet_fire_timer = new_timer(60),
                 spawn_bullets = function(enemy_movement, player_collision_circle)
                     local bullets = {}
-                    add(bullets, _m.enemy_bullet_factory(
-                        new_movement_line_factory {
-                            base_speed_y = enemy_movement.speed_xy.y,
-                            angle = .75,
-                            angled_speed = .5,
-                        }(enemy_movement.xy.plus(0, 3))
-                    ))
+                    for i = 4, 4 do
+                        add(bullets, enemy_bullet_factory(
+                            new_movement_line_factory {
+                                base_speed_y = enemy_movement.speed_xy.y,
+                                angle = .5 + i / 16,
+                                angled_speed = 2,
+                            }(enemy_movement.xy)
+                        ))
+                    end
                     return bullets
                 end,
-                movement_factory = new_movement_fixed_factory(),
+                powerups_distribution = "t",
+            }
+        end
+        assert(false, "unexpected enemy_map_marker = " .. enemy_map_marker)
+    end
+
+    function _m.boss_properties()
+        return {
+            health = 25,
+            sprite = new_static_sprite(56, 26, 4, 98),
+            collision_circles = function(movement)
+                return {
+                    { xy = movement.xy.plus(0, 3), r = 5 },
+                }
+            end,
+            phases = {
+                -- phase 1:
+                {
+                    triggering_health_fraction = 1,
+                    bullet_fire_timer = new_timer(80),
+                    spawn_bullets = function(enemy_movement, player_collision_circle)
+                        local bullets = {}
+                        add(bullets, enemy_bullet_factory(
+                            new_movement_line_factory {
+                                base_speed_y = enemy_movement.speed_xy.y,
+                                angle = .75,
+                                angled_speed = .5,
+                            }(enemy_movement.xy.plus(0, 3))
+                        ))
+                        return bullets
+                    end,
+                    movement_factory = new_movement_fixed_factory(),
+                },
             },
-        },
-    }
+        }
+    end
+
 end
--- src/mission_3.lua
+++ src/mission_3.lua
@@ -4,84 +4,130 @@
 
 _m = {
     mission_number = 3,
-    scroll_per_frame = 1.5,
+    scroll_per_frame = 1,
     bg_color = _color_2_darker_purple,
-    has_bg_tiles = false,
     mission_name = "rotfl",
     boss_name = "lol"
 }
 
--- TODO: polish mission 3 enemy bullets
-_m.enemy_bullet_factory = new_enemy_bullet_factory {
-    bullet_sprite = new_static_sprite(4, 4, 124, 64),
-    collision_circle_r = 1.5,
-}
+do
+    local particles
 
-function _m.enemy_properties_for(enemy_map_marker)
-    if enemy_map_marker == 73 then
-        return {
-            health = 1,
-            ship_sprite = new_static_sprite(8, 8, 0, 64),
-            collision_circle_r = 3.5,
-            collision_circle_offset_y = 0,
-            movement_factory = new_movement_sequence_factory {
-                new_movement_line_factory {
-                    frames = 80,
-                    angle = .75,
-                    angled_speed = .5,
-                },
-                new_movement_line_factory {
-                    angle = .75,
-                    angled_speed = 1.5,
-                },
-            },
-            bullet_fire_timer = new_timer(30),
-            spawn_bullets = function(enemy_movement, player_collision_circle)
-                local bullets = {}
-                for i = 3, 5 do
-                    add(bullets, _m.enemy_bullet_factory(
-                        new_movement_line_factory {
-                            base_speed_y = enemy_movement.speed_xy.y,
-                            angle = .5 + i / 16,
-                            angled_speed = 2,
-                        }(enemy_movement.xy)
-                    ))
-                end
-                return bullets
-            end,
-            powerups_distribution = "t",
-        }
-    end
-    assert(false, "unexpected enemy_map_marker = " .. enemy_map_marker)
-end
-
-function _m.boss_properties()
-    return {
-        health = 20,
-        sprite = new_static_sprite(56, 26, 4, 98),
-        collision_circles = function(movement)
-            return {
-                { xy = movement.xy.plus(0, 3), r = 5 },
+    local function maybe_add_particle(y)
+        if rnd() < .1 then
+            local props = rnd {
+                { sx = 40, sy = 32, w = 5, h = 5 },
+                { sx = 45, sy = 32, w = 4, h = 3 },
+                { sx = 45, sy = 35, w = 3, h = 4 },
             }
-        end,
-        phases = {
-            -- phase 1:
-            {
-                triggering_health_fraction = 1,
-                bullet_fire_timer = new_timer(80),
+            add(particles, {
+                xy = _xy(
+                    ceil(.1 + rnd(_gaw - .1)),
+                    y
+                ),
+                sprite = new_static_sprite(props.w, props.h, props.sx, props.sy)
+            })
+        end
+    end
+
+    function _m.level_bg_init()
+        particles = {}
+
+        for y = 0, _gah - 1 do
+            maybe_add_particle(y)
+        end
+    end
+
+    function _m.level_bg_update()
+        for _, particle in pairs(particles) do
+            particle.xy = particle.xy.plus(0, _m.scroll_per_frame)
+            if particle.xy.y >= _gah + _ts then
+                del(particles, particle)
+            end
+        end
+
+        maybe_add_particle(-_ts)
+    end
+
+    function _m.level_bg_draw(min_visible_distance, max_visible_distance)
+        for _, particle in pairs(particles) do
+            particle.sprite._draw(particle.xy)
+        end
+    end
+
+    -- TODO: polish mission 3 enemy bullets
+    local enemy_bullet_factory = new_enemy_bullet_factory {
+        bullet_sprite = new_static_sprite(4, 4, 124, 64),
+        collision_circle_r = 1.5,
+    }
+
+    function _m.enemy_properties_for(enemy_map_marker)
+        if enemy_map_marker == 73 then
+            return {
+                health = 1,
+                ship_sprite = new_static_sprite(8, 8, 0, 64),
+                collision_circle_r = 3.5,
+                collision_circle_offset_y = 0,
+                movement_factory = new_movement_sequence_factory {
+                    new_movement_line_factory {
+                        frames = 80,
+                        angle = .75,
+                        angled_speed = .5,
+                    },
+                    new_movement_line_factory {
+                        angle = .75,
+                        angled_speed = 1.5,
+                    },
+                },
+                bullet_fire_timer = new_timer(30),
                 spawn_bullets = function(enemy_movement, player_collision_circle)
                     local bullets = {}
-                    add(bullets, _m.enemy_bullet_factory(
-                        new_movement_line_factory {
-                            base_speed_y = enemy_movement.speed_xy.y,
-                            angle = .75,
-                            angled_speed = .5,
-                        }(enemy_movement.xy.plus(0, 3))
-                    ))
+                    for i = 3, 5 do
+                        add(bullets, enemy_bullet_factory(
+                            new_movement_line_factory {
+                                base_speed_y = enemy_movement.speed_xy.y,
+                                angle = .5 + i / 16,
+                                angled_speed = 2,
+                            }(enemy_movement.xy)
+                        ))
+                    end
                     return bullets
                 end,
-                movement_factory = new_movement_fixed_factory(),
+                powerups_distribution = "t",
+            }
+        end
+        assert(false, "unexpected enemy_map_marker = " .. enemy_map_marker)
+    end
+
+    function _m.boss_properties()
+        return {
+            health = 20,
+            sprite = new_static_sprite(56, 26, 4, 98),
+            collision_circles = function(movement)
+                return {
+                    { xy = movement.xy.plus(0, 3), r = 5 },
+                }
+            end,
+            phases = {
+                -- phase 1:
+                {
+                    triggering_health_fraction = 1,
+                    bullet_fire_timer = new_timer(80),
+                    spawn_bullets = function(enemy_movement, player_collision_circle)
+                        local bullets = {}
+                        add(bullets, enemy_bullet_factory(
+                            new_movement_line_factory {
+                                base_speed_y = enemy_movement.speed_xy.y,
+                                angle = .75,
+                                angled_speed = .5,
+                            }(enemy_movement.xy.plus(0, 3))
+                        ))
+                        return bullets
+                    end,
+                    movement_factory = new_movement_fixed_factory(),
+                },
             },
-        },
-    }
+        }
+    end
+
 end
