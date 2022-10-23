353148d1a7f7ad11d694a37a784a1f9cf17e2d0d
Beetroot Paul <paul@beetrootpaul.com>
2022-10-23 17:00:10 +0200

    formatted with pico8-ls 68eb463bb3fb72e06223f5ac08eb4839fb3c6c0f

--- src/cart_main.lua
+++ src/cart_main.lua
@@ -1,39 +1,29 @@
 -- -- -- -- -- -- --
 -- cart_main.lua  --
 -- -- -- -- -- -- --
-
 _game_version = "V0.3.0"
-
 local current_screen, next_screen
-
 function _init()
     local preselected_mission_number = _get_cart_param(1)
     -- DEBUG:
     --preselected_mission_number = 1
-
     -- to clear cart data, go to cart data folder (defined as "root_path" in "$HOME/Library/Application Support/pico-8/config.txt")
     -- and delete "brp_dart-07.p8d.txt" file
     cartdata("brp_dart-07")
-
-    current_screen = preselected_mission_number and
-        new_screen_title(preselected_mission_number, true, false, false) or
-        new_screen_brp()
+    current_screen = preselected_mission_number and new_screen_title(preselected_mission_number, true, false, false) or new_screen_brp()
     -- DEBUG:
     --current_screen = new_screen_controls(preselected_mission_number)
     --current_screen = new_screen_title(preselected_mission_number, false, true, false)
     --current_screen = new_screen_select_mission(preselected_mission_number)
-
     current_screen._init()
 end
 
 function _update60()
     next_screen = current_screen._post_draw()
-
     if next_screen then
         current_screen = next_screen
         current_screen._init()
     end
-
     current_screen._update()
 end
 
@@ -46,22 +36,17 @@ end
 -- TODO: polishing: music: mission 2 boss
 -- TODO: polishing: music: mission 3
 -- TODO: polishing: music: mission 3 boss
-
 -- TODO: polishing: sprites: mission 2: enemies 
 -- TODO: polishing: sprites: mission 2: boss 
 -- TODO: polishing: sprites: mission 3: enemies 
 -- TODO: polishing: sprites: mission 3: boss 
-
 -- TODO: balancing: powerup distributions: mission 2
 -- TODO: balancing: powerup distributions: mission 3
 -- TODO: balancing: mission 2: enemy types, health, speed, their bullets: timer, speed, amount, angles, timer, SFX or not 
 -- TODO: balancing: mission 3: enemy types, health, speed, their bullets: timer, speed, amount, angles, timer, SFX or not 
-
 -- TODO: carts: label images: mission 2
 -- TODO: carts: label images: mission 3
-
 -- TODO: check if no DEBUG code is left uncommented
-
 -- TODO: consider this left-right enemy type for mission 2
 -- enemy: left-right
 --[76] = {
